54ea981d117d2ca31f385e770a928cbed7087715
Anik mahmud <96568968+AnikMahmud78@users.noreply.github.com>
2022-03-18 10:57:10 +0600

    name

--- Registration.js
+++
@@ -1,7 +0,0 @@
-function showInput(){
-   const show = document.querySelector("#showInput");
-   show.innerHTML=`
-   <p class="input__tittle m-1 mt-3">Enter OTP</p>
-   <input class="input__field " type="text">
-   `
-}
